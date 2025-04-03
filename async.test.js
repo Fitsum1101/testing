263a2cb22330634973eba51fb3c20cd99d9096d6
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-03 22:03:41 +0300

    promise demo

--- async.test.js
+++ async.test.js
@@ -1,10 +1,11 @@
 import { expect, it } from "vitest";
 import { generateToken } from "./async";
-it("should return valid token when the user give valid data", () => {
+it("should return valid token when the user give valid data", (done) => {
   const email = "test@test.com";
 
   generateToken(email, async (err, token) => {
-    expect(token).toBeDefined(2);
-    done();
+    expect(token).toBeDefined();
   });
 });
+
+
