eda0b346cc350af029ba22aeb7c9c4a7d580cb88
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-03 22:48:28 +0300

    promise projects

--- writeFile.js
+++ writeFile.js
@@ -1,12 +1,44 @@
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("after rejected");
-  }, 1000);
-  reject("error");
-});
+// const myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("after rejected");
+//   }, 1000);
+//   reject("error");
+// });
 
-myPromise
-  .then((result) => console.log(result))
-  .catch((err) => {
-    console.log(err);
-  });
+// myPromise
+//   .then((result) => console.log(result))
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// const newPromise = new Promise((resolve, reject) => {
+//   reject("something is wrong");
+//   resolve(() => {
+//     return new Promise((innerResolve) => {
+//       innerResolve("inner resolve");
+//     });
+//   });
+// });
+
+// newPromise
+//   .then((result) => {
+//     return result();
+//   })
+//   .then((finalResult) => {
+//     console.log(finalResult);
+//   })
+//   .catch((err) => console.log(err));
+
+// Promise.all([1, 2, Promise.reject("rejected for some reason!!")])
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+
+const promise1 = Promise.resolve(3);
+const promise2 = new Promise((resolve, reject) =>
+  setTimeout(reject, 100, "foo")
+);
+const promises = [promise1, promise2];
+
+Promise.allSettled(promises).then((results) =>
+  results.forEach((result) => console.log(result.status))
+);
