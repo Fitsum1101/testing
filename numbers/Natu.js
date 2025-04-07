759f4117a02ed8ac2648729538336d4d76103477
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-07 20:08:25 +0300

    making things for testing

---
+++ numbers/Natu.js
@@ -0,0 +1,15 @@
+import path from "path";
+
+export function NathuralNumbers(num) {
+  if (num > 0) {
+    return num;
+  }
+}
+
+const newFunction = () => {
+  console.log("new content");
+};
+
+const anotherfunction = () => {
+  return "amazing product";
+};
