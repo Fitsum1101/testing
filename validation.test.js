27ec74bdd29cf0f010fe5b3565c5dffc7d163c32
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-11 16:40:05 +0300

    adding validation file

--- validation.test.js
+++ validation.test.js
@@ -78,3 +78,6 @@ it.fails("decimals are not equal in javascript", () => {
   // console.log(0.1 + 0.2 === 0.3);
   expect(0.2 + 0.1).toBe(0.3); // 0.2 + 0.1 is 0.30000000000000004
 });
+function heelo() {
+  console.log("hi");
+}
