bc95882adf5c2ffdb2b1706d2eff80968f89df88
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-15 21:39:41 +0300

    adding the add file

--- async.js
+++ async.js
@@ -1,6 +1,7 @@
 import { sign } from "jsonwebtoken";
 
 export const generateToken = (email, cb) => {
-  return sign({email}, "mysecret", cb);
+  return sign({ email }, "mysecret", cb);
 };
 
+const i = 3;
