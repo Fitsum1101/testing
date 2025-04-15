9d49cf753b08b4b5b642567903687ddffbbfdb68
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-15 21:40:15 +0300

    adding the add file

--- async.test.js
+++ async.test.js
@@ -1,5 +1,4 @@
 import { expect, it } from "vitest";
-import { generateToken } from "./async";
 it("should return valid token when the user give valid data", (done) => {
   const email = "test@test.com";
 
