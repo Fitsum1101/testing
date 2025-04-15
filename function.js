e0de2d1e355110bd619387a24c62cd3242a02537
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-15 21:40:25 +0300

    adding the add file

--- function.js
+++ function.js
@@ -2,9 +2,3 @@ import path from "path";
 
 console.log(path.parse(process.cwd() + "/add-number.js"));
 const newFunction = new Function();
-function heelo() {
-    console.log("hi");
-  }
-  function heelo() {
-    console.log("hi");
-  }
