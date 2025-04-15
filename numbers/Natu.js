546f6e69609c0f419c64b44ee6516d3ba82027c9
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-15 21:39:27 +0300

    adding the add file

--- numbers/Natu.js
+++ numbers/Natu.js
@@ -6,20 +6,3 @@ export function NathuralNumbers(num) {
   }
 }
 
-const newFunction = () => {
-  console.log("new content");
-};
-
-const anotherfunction = () => {
-  return "amazing product";
-};
-
-
-
-function heelo() {
-  console.log("hi");
-}
-
-
-const updatedSucc= 0;
-const newFuctionality = 0;
