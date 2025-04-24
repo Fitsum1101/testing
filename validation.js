40c4b8575690405dbd86fdde8c3fe48ae1433eb6
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-24 19:55:22 +0300

    adding numbwr update

--- validation.js
+++ validation.js
@@ -4,12 +4,7 @@ function validateStringNotEmpty(value) {
     }
   }
   
-  function validateNumber(number) {
-    if (isNaN(number)) {
-      throw new Error('Invalid number input.');
-    }
-  }
-  
+ 
 
 
   
