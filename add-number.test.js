5d5c94f6f548a25986c50900042ffb9ab3bf806a
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-24 19:52:54 +0300

    adding numbwr update

--- add-number.test.js
+++ add-number.test.js
@@ -4,12 +4,6 @@ it("should return 0 if it called with out paramiters", function () {
   const result = sum();
 });
 
-it("should return a sum if one of them is a string ", function () {
-  const a = "1";
-  const b = 2;
-  const result = sum(a, b);
-  expect(result).toBeTypeOf("number");
-});
 
 it("should return invalid if the parameters are other than string and number", function () {
   const a = {};
