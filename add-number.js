373ce6a123941201cf6007bbc2a5bb14c69d9ca9
Fitsum Kifle <fitsumt835@gmail.com>
2025-05-04 19:27:20 +0300

    amazing texst

--- add-number.js
+++ add-number.js
@@ -7,3 +7,13 @@ const number = () => {
 }
 
 export default add-number
+
+
+const newPromise = new Promise((resolve, reject) => {
+  reject("something is wrong");
+  resolve(() => {
+    return new Promise((innerResolve) => {
+      innerResolve("inner resolve");
+    });
+  });
+});
