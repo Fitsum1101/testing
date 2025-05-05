0d93f27f3e298b2e23cbd1c23933197877486c49
Fitsum Kifle <fitsumt835@gmail.com>
2025-05-05 20:26:11 +0300

    amazing java scrit

--- add-number.js
+++ add-number.js
@@ -9,11 +9,3 @@ const number = () => {
 export default add-number
 
 
-const newPromise = new Promise((resolve, reject) => {
-  reject("something is wrong");
-  resolve(() => {
-    return new Promise((innerResolve) => {
-      innerResolve("inner resolve");
-    });
-  });
-});
