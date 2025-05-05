43cbe967c8ac2f17e3939986aeac2fd48d7c3603
Fitsum Kifle <fitsumt835@gmail.com>
2025-05-05 20:27:58 +0300

    amazing java scrit

--- async.test.js
+++ async.test.js
@@ -1,10 +1,2 @@
 import React from 'react'
 
-const newPromise = new Promise((resolve, reject) => {
-  reject("something is wrong");
-  resolve(() => {
-    return new Promise((innerResolve) => {
-      innerResolve("inner resolve");
-    });
-  });
-});
