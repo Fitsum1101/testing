9245a13be811f8f69d17b93df85ff6b2fb10cbe2
Fitsum Kifle <fitsumt835@gmail.com>
2025-05-22 16:27:21 +0300

    very hand trick

--- add-number.test.js
+++
@@ -1,8 +0,0 @@
-import { sum } from "./add-number";
-import { it, expect, describe } from "vitest";
-it("should return 0 if it called with out paramiters", function () {
-  const result = sum();
-});
-
-const p = 4567;
-const num = "020203202";
